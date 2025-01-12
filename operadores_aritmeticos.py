5db779cb44727688454d4d3c259490c02cebf0fd
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-01-12 14:29:36 -0300

    desenvolvimentod ce sistemas com logica com operações aritmweticas

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,41 @@
+print(1)
+
+print(10 + 20)
+
+print(30 + 40)
+
+print(10 + 70 + 90 + 1100 + 10000)
+
+print(123 - 87)
+
+print(67 - 198)
+
+print(34 - 78 - 90 - 167)
+
+print(10 * 8)
+
+print(12 * 3 * 5)
+
+print(100 / 10)
+
+print(129 / 23)
+
+print(129 // 23)
+
+print(1345 / 2337)
+
+print(129 % 23)
+
+print(10 / 3)
+
+print( 10 ** 2 )
+print( 10 ** 4 )
+print(3 ** 3)
+
+print(10 + 2 * 10)
+
+print((10 + 2) * 10)
+
+print( 12 + 3 - 1 * 50 / 3 )
+
+print( (12 + 3 - 1) * 50 / 3 )
