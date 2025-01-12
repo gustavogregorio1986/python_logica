6d3dd2d1c5e4f6786720085479ccd08550d1c05f
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-01-12 15:12:28 -0300

    desenvolvimento de logica de programação, com operadores relacionais logicos

---
+++ operadores_relacionais_logicos.py
@@ -0,0 +1,29 @@
+print(10 > 20)
+
+print(10 < 20)
+
+nome_aluno = 'Vinicius'
+nome_professor = 'Vinicius'
+
+print(nome_aluno == nome_professor)
+
+nome_aluno0 = 'vinicius'
+nome_professor00 = 'Vinicius'
+
+print(nome_aluno0 == nome_professor00)
+
+print(10 != 10)
+
+print((10 > 20) and (20 > 30))
+
+print((10 < 20) and (20 < 30))
+
+print((1 < 2) or (3 > 6))
+
+print(('Vinicius' == 'Vinicius') or ('Python' != 'python'))
+
+print(True)
+
+print(not True)
+
+print(not False)
---
+++ variaveis.py
@@ -0,0 +1,62 @@
+print('Vinicius')
+
+##Print('Vinicius')
+
+nome_aluno = 'Vinicius'
+
+print(nome_aluno)
+
+numero1 = 123
+
+print(numero1)
+
+##34aluno = 123 erro de sintaxe
+
+nome_aluno  = 'Gabriel'
+
+print(nome_aluno)
+
+Nome_aluno = 'Vinicius'
+
+print(Nome_aluno)
+
+numero_telefone = 775438765
+
+print(numero_telefone)
+
+numero_cpf = 797647564
+
+print(numero_cpf)
+
+print(type(numero_cpf))
+
+print(type(123.45))
+
+altura = 1.80
+
+print(type(altura))
+
+peso = 80.67
+
+print(type(peso))
+
+nome_professor = 'Vinicius'
+
+print(nome_professor)
+
+print(True)
+
+print(False)
+
+status = True
+
+print(type(status))
+
+numero01 = 1000
+numero02 = 1000
+numero03 = 1000
+
+soma = numero01 + numero02 + numero03
+
+print(soma)
+
