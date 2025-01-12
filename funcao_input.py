4d85bb9b8325b53160d712b6871a91c05793607c
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-01-12 00:27:08 -0300

    desnevolvimento para revisar a materia para entender melhor e envoluir como profissional

---
+++ funcao_input.py
@@ -0,0 +1,18 @@
+input('Digite o sue nome: ')
+
+print( input('Digite o seu nome: ') )
+
+print('Vinicius')
+
+nome = input('Digite o seu nome: ')
+
+print(nome)
+
+numero1 = input('Diogite o primeiro numero: ')
+
+numero2 = input('Diogite o segundo numero: ')
+
+print(numero1)
+print(numero2)
+
+print( int(numero1) + int(numero2) )
