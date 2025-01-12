e4c76758c061cac0fb138d1e72f9e1de00927906
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-01-12 00:17:05 -0300

    desenvolvime para fixar o python e ir estudando

---
+++ funcao_print.py
@@ -0,0 +1,20 @@
+print("Meu primeiro print!")
+
+print('Meu primeiro print!')
+
+print("D' agua")
+
+##print('d 'agua) erro não aceita desse jetio o print 
+
+print(10 * 300)
+
+print(125 * 4)
+
+print(12345)
+
+nome = 'Vinicius'
+
+print(nome)
+
+print('Vinicius',10000, 'Python',12 * 56)
+
---
+++ meu_primeiro_programa.py
@@ -0,0 +1,6 @@
+print(1)
+
+for i in range(10):
+    print(i)
+
+print("Meu primeiro print!")
