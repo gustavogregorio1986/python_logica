d6a0393d9a1519e6c203fdfa1fba8bcb2653a66b
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-01-13 20:59:49 -0300

    desenvolvimento de manipulação de string para evoluir no python

---
+++ manipulacao_string.py
@@ -0,0 +1,41 @@
+frase = 'Meu nome é Vincius. Eu estou programando a linguagem Python'
+
+print(frase)
+
+print(frase.split())
+
+print(frase.split('.'))
+
+print('Meu nome é Vincius. Eu estou programando a linguagem Python'.split())
+
+print(type('Meu nome é Vincius. Eu estou programando a linguagem Python'.split()))
+
+frase = '     Meu nome é Vinicius    '
+
+print(frase)
+
+print(frase.strip())
+
+print('Vinicius' == 'Vinicius ')
+
+print('Vinicius' == 'Vinicius '.strip())
+
+frase = 'Meu nome é Vinicius e eue gosto de esytudar python.'
+
+print(frase.capitalize())
+
+frase = 'Meu nome é Vincius. Eu estou programando a linguagem Python'
+
+print(frase.lower())
+
+print(frase)
+
+frase = frase.lower()
+print(frase)
+
+frase = 'Meu nome é Vincius. Eu estou programando a linguagem Python'
+
+print(frase.upper())
+
+frase = 'Meu nome é Vincius. Eu estou programando a linguagem Python'
+print(frase.title())
