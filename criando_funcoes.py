121313d8ae6a58c6d43c622f730509ad5fcf2120
gustavo gregorio <gustavogregorio1986@gmail.com>
2025-01-15 20:44:09 -0300

    desnevolvimento de funções em pyhton na logica de programação

---
+++ criando_funcoes.py
@@ -0,0 +1,27 @@
+print('Seja bem vindo à eEmpowerdata!')
+print('Você está logado(a) no sistema!')
+print('Nõa esqueça de fazer o logout!')
+
+print('Função para aprender')
+
+def imprimir_frase():
+    print('Seja bem vindo à eEmpowerdata!')
+    print('Você está logado(a) no sistema!')
+    print('Nõa esqueça de fazer o logout!')
+
+imprimir_frase()
+
+def imprimir_frases_usuario(nome):
+    print(f'Seja bem vindo(a) à Empowerdata, {nome}')
+    print(f'Você está logado(a) nom sistema {nome}!')
+    print(f'Não esqueça de fazer o logout {nome}!')
+
+imprimir_frases_usuario('Vinicius')
+
+def somar_numeros(numero1, numero2):
+    print(numero1 + numero2)
+
+somar_numeros(10, 15)
+
+somar_numeros(1000, 15)
+
